0ad0ab7502777c7e8700e751424c4191a3eb4eba
binpoem <binpoem@gmail.com>
2024-01-29 14:02:13 +0800

    Flat shading render

--- main.cpp
+++ main.cpp
@@ -106,8 +106,28 @@ void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
 int main(int argc, char** argv) {
   TGAImage image(width, height, TGAImage::RGB);
 
-  Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
-  triangle(t0, image, red);
+  Vec3f light_dir(0, 0, -1);  // define light_dir
+  auto model = new Model{"../obj/african_head.obj"};
+  for (int i = 0; i < model->nfaces(); i++) {
+    std::vector<int> face = model->face(i);
+    Vec2i screen_coords[3];
+    Vec3f world_coords[3];
+    for (int j = 0; j < 3; j++) {
+      Vec3f v = model->vert(face[j]);
+      screen_coords[j] =
+          Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+      world_coords[j] = v;
+    }
+    Vec3f n = (world_coords[2] - world_coords[0]) ^
+              (world_coords[1] - world_coords[0]);
+    n.normalize();
+    float intensity = n * light_dir;
+    if (intensity > 0) {
+      triangle(
+          screen_coords, image,
+          TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+    }
+  }
   image.flip_vertically();
   image.write_tga_file("output.tga");
   return 0;
