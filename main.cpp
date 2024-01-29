9bb0263e4f7151e970bcaa8adc71df323887aee5
binpoem <binpoem@gmail.com>
2024-01-29 12:11:35 +0800

    2: Triangle rasterization

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 0, 255, 255);
 const int width = 800;
 const int height = 800;
 
@@ -37,22 +38,76 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
     }
   }
 }
+void line(Vec2i t0, Vec2i t1, TGAImage& image, TGAColor color) {
+  line(t0.x, t0.y, t1.x, t1.y, image, color);
+}
+// !Old-school method: Line sweeping
+// void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
+// {
+//   if (t0.y == t1.y && t0.y == t2.y)
+//     return;  // I dont care about degenerate triangles
+//   // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!)
+//   if (t0.y > t1.y) std::swap(t0, t1);
+//   if (t0.y > t2.y) std::swap(t0, t2);
+//   if (t1.y > t2.y) std::swap(t1, t2);
+//   int total_height = t2.y - t0.y;
+//   for (int i = 0; i < total_height; i++) {
+//     bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+//     int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+//     float alpha = (float)i / total_height;
+//     float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) /
+//                  segment_height;  // be careful: with above conditions no
+//                                   // division by zero here
+//     Vec2i A = t0 + (t2 - t0) * alpha;
+//     Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+//     if (A.x > B.x) std::swap(A, B);
+//     for (int j = A.x; j <= B.x; j++) {
+//       image.set(j, t0.y + i,
+//                 color);  // attention, due to int casts t0.y+i != A.y
+//     }
+//   }
+// }
+
+Vec3f barycentric(Vec2i* pts, Vec2i P) {
+  auto AB = pts[1] - pts[0];
+  auto AC = pts[2] - pts[0];
+  auto PA = pts[0] - P;
+
+  Vec3f u = Vec3f(AB.x, AC.x, PA.x) ^ Vec3f(AB.y, AC.y, PA.y);
+  /* `pts` and `P` has integer value as coordinates
+     so `abs(u[2])` < 1 means `u[2]` is 0, that means
+     triangle is degenerate, in this case return something with negative
+     coordinates */
+  if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
+  return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
+  Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+  Vec2i bboxmax(0, 0);
+  Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+  for (int i = 0; i < 3; i++) {
+    bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+    bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+    bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+    bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+  }
+  Vec2i P;
+  for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+    for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+      Vec3f bc_screen = barycentric(pts, P);
+      if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+      image.set(P.x, P.y, color);
+    }
+  }
+}
 
 int main(int argc, char** argv) {
   TGAImage image(width, height, TGAImage::RGB);
-  auto model = new Model{"../obj/african_head.obj"};
-  for (int i = 0; i < model->nfaces(); i++) {
-    std::vector<int> face = model->face(i);
-    for (int j = 0; j < 3; j++) {
-      Vec3f v0 = model->vert(face[j]);
-      Vec3f v1 = model->vert(face[(j + 1) % 3]);
-      int x0 = (v0.x + 1.) * width / 2.;
-      int y0 = (v0.y + 1.) * height / 2.;
-      int x1 = (v1.x + 1.) * width / 2.;
-      int y1 = (v1.y + 1.) * height / 2.;
-      line(x0, y0, x1, y1, image, white);
-    }
-  }
+
+  Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+  triangle(t0, image, red);
   image.flip_vertically();
   image.write_tga_file("output.tga");
   return 0;
